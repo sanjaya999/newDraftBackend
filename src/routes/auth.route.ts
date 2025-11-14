3a987ccd219162d31dd227efdc0551cfa55a760d
san <poudelsanjaya12@gmail.com>
2025-11-14 11:19:35 +0545

    feat: add validation to user registration route using Zod schema

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { registerController } from "../api/auth.controller.js";
+import { validate } from "../middleware/validate.middleware.js";
+import { registerSchema } from "../schemas/auth.schema.js";
 
 const authRouter = Router();
 
-authRouter.post("/register" , registerController)
+authRouter.post("/register" , validate(registerSchema),registerController)
 
 export default authRouter;
