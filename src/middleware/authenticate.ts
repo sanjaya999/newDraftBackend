ef45e0ffb334dc193eaec2604b1a988abc68af06
san <poudelsanjaya12@gmail.com>
2025-11-17 08:47:40 +0545

    added auth for sockets

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 import { env } from "../infrastructure/envConfig.js";
 import  { asyncHandler } from "./asyncHandler.js";
 import { findUserById } from "../repository/user.repository.js";
+import type { Socket } from "socket.io";
 
 export const authenticate = asyncHandler(async(req:Request , res:Response , next:NextFunction)=>{
 
@@ -24,3 +25,26 @@ export const authenticate = asyncHandler(async(req:Request , res:Response , next
         next();
 }
 )
+
+export async function socketAuth(socket: Socket, next:(err? : ApiError)=>void){
+    try{
+        const token = socket.handshake.auth.token ||
+        socket.handshake.headers.authorization?.replace("Bearer ","") ||
+        socket.handshake.query.token as string;
+
+            if (!token) {
+                return next(new ApiError('Socket requires auth token' , StatusCodes.UNAUTHORIZED));
+            }
+
+        const decodedToken = jwt.verify(token, env.JWT_ACCESS_SECRET) as jwt.JwtPayload;
+        const user = await findUserById(decodedToken?.id);
+         if(!user){
+            throw new ApiError("Invalid Access Token" , StatusCodes.UNAUTHORIZED)
+        }
+        socket.data.user = user;
+        console.log("conencting user ,", socket.data.user)
+        next();
+    }catch(error){
+        return next(new ApiError('Socket requires auth token' , StatusCodes.UNAUTHORIZED));
+    }
+}
