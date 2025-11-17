019455df644a1a244927eda31c367cedc486d9e7
san <poudelsanjaya12@gmail.com>
2025-11-17 15:40:20 +0545

    added socket and join documents

--- src/sockets/socket.server.ts
+++ src/sockets/socket.server.ts
@@ -3,6 +3,9 @@ import type { DocumentData } from "../types/documents.js";
 import { env } from "../infrastructure/envConfig.js";
 import { StatusCodes } from "http-status-codes";
 import { socketAuth } from "../middleware/authenticate.js";
+import { logger } from "../infrastructure/logger.js";
+import { joinDocument, persistDocument } from "../utils/document.manager.js";
+import { success } from "zod";
 
 const documents= new Map<string, DocumentData>();
 
@@ -28,7 +31,57 @@ function setupSocketHandlers(io: Server){
     io.use(socketAuth);
 
     io.on('connection' , (socket: Socket) =>{
-        console.log(`user connected: ${socket.data.user.id}`)
+        const userId = socket.data.user.id;
+        const socketId = socket.id;
 
+        logger.info(`Socket ${socketId} connected (User ${userId})`);
+
+        socket.on("document:join" , async(docID: string, callback)=>{
+            try{
+                socket.join(docID);
+                const result = await joinDocument(docID, userId , socketId);
+                if(result.success){
+                    callback({
+                        success:true,
+                        state: result.state,
+                    })
+                    logger.info(`User ${userId} joined doc ${docID}`);
+                }else {
+                    callback({ success: false, error: result.error });
+                }
+            }catch (err : any) {
+                logger.error(`Error in document:join:`, err);
+                callback({ success: false, error: "Server error" });
+    }
+        })
+
+        socket.on('disconnect' , (reason)=>{
+            logger.info(`Socket ${socketId} disconnected`);
+            socket.rooms.forEach((docID)=>{
+                if(docID === socket.id)return;
+                const docData = documents.get(docID);
+                if(docData){
+                    docData.connections.delete(socket.id);
+                    logger.info(`Removed ${socketId} from doc ${docID}`);
+                }
+                if (docData?.connections.size === 0) {
+                    logger.info(`Document ${docID} is now empty. Persisting and removing.`);
+          
+                    persistDocument(docID);
+          
+                    docData.ydoc.destroy(); 
+                        documents.delete(docID);
+                }
+            })
+        })
+
+        socket.on('error', (error) => {
+            console.error('Socket error:', error);
+        });
+
+        io.engine.on('connection_error', (err) => {
+            console.error(' Connection error:', err);
+        });
     })
+
 }
