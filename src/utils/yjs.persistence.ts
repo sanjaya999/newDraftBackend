30411f675b1b5a9aeda272d1deecb55a483c13f9
san <poudelsanjaya12@gmail.com>
2025-11-17 15:39:36 +0545

    changed schema and yjs persistance

---
+++ src/utils/yjs.persistence.ts
@@ -0,0 +1,27 @@
+import * as Y from  'yjs'
+import { prisma } from '../infrastructure/database.js'
+import { logger } from '../infrastructure/logger.js';
+
+export async function loadYjsDocument(
+    docID: string,
+): Promise<Uint8Array | null>{
+    try{
+        const document = await prisma.document.findUnique({
+            where: {
+                id: docID
+            },
+            select: {
+                content:true
+            },
+        });
+        if(!document?.content){
+            logger.info(`no save state found for doc ${docID}`);
+            return null;
+        }
+        return new Uint8Array(document.content);
+
+    } catch (err) {
+        console.error('Error loading Yjs document:', err);
+    return null;
+  }
+}
